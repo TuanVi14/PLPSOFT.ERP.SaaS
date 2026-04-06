af8b92fd3f41fe112beef3888a31c212d369f719
TuanVi14 <phamtrantuanvi@outlook.com>
2026-04-06 09:19:10 +0700

    Delete PLPSOFT.ERP.WebApp/Models/ProductUnitViewModel.cs

--- PLPSOFT.ERP.WebApp/Models/ProductUnitViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PLPSOFT.ERP.WebApp.Models
-{
-    public class ProductUnitViewModel
-    {
-        public long ProductUnitID { get; set; } // Khớp tên với Entity
-        public string UnitCode { get; set; } = string.Empty;
-        public string UnitName { get; set; } = string.Empty;
-    }
-}
