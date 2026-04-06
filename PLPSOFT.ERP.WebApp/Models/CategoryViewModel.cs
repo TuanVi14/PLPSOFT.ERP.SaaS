2ad6b4cefcb22f9f2dfe1d027ba3c388f3032a16
TuanVi14 <phamtrantuanvi@outlook.com>
2026-04-06 09:18:57 +0700

    Delete PLPSOFT.ERP.WebApp/Models/CategoryViewModel.cs

--- PLPSOFT.ERP.WebApp/Models/CategoryViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PLPSOFT.ERP.WebApp.Models
-{
-    public class CategoryViewModel
-    {
-        public long CategoryID { get; set; } // Đổi sang long
-        public string CategoryCode { get; set; } = string.Empty;
-        public string CategoryName { get; set; } = string.Empty;
-    }
-}
