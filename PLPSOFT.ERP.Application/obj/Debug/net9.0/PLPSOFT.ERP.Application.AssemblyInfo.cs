6770f75fec8fba06d1dc1ce750ee2922df39afbb
NhatHuy <huytrac2323@gmail.com>
2026-04-07 10:21:03 +0700

    update

--- PLPSOFT.ERP.Application/obj/Debug/net9.0/PLPSOFT.ERP.Application.AssemblyInfo.cs
+++ PLPSOFT.ERP.Application/obj/Debug/net9.0/PLPSOFT.ERP.Application.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PLPSOFT.ERP.Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+047113555fe91231e90ce9bf43ab61c3201c485f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff4aa280c8ac7d94a5b99b92b5c0988fec2e96d1")]
 [assembly: System.Reflection.AssemblyProductAttribute("PLPSOFT.ERP.Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PLPSOFT.ERP.Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
